1d68dcf726a331a96cd0f5d979db5d10b17a0594
MAZE <remvze@proton.me>
2025-12-30 19:10:28 +0330

    style: minor change

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -24,12 +24,10 @@ export function Modal({
     modal: {
       hidden: {
         opacity: 0,
-        scale: 0.9,
         y: 20,
       },
       show: {
         opacity: 1,
-        scale: 1,
         y: 0,
       },
     },
